0162b9cdad1e57c6501d4608ecd9231b996b64a2
logic-three-body <1552451580@qq.com>
2021-03-02 16:36:25 +0800

    实现高斯模糊

--- OPENCV_PROCESS/cv_process3.py
+++ OPENCV_PROCESS/cv_process3.py
@@ -1,14 +1,15 @@
-#练习 2. 从摄像头读取视频并保存其中某一帧
+#练习 3. 在 opencv窗口实时显示高斯模糊后的（彩色）图像
 import cv2
 print(cv2.__file__)
 cap = cv2.VideoCapture(0)#注意如果为1imshow会崩溃
 while True:
     ret, im = cap.read()
-    cv2.imshow('video test', im)
+    blur = cv2.GaussianBlur(im, (0, 0), 5)
+    cv2.imshow('video test', blur)
     key = cv2.waitKey(10)
     if key == 27:
         break
     if key == ord(' '):
-        cv2.imwrite('vid_result.jpg',im)
+        cv2.imwrite('vid_blur_result.jpg',blur)
 cap.release()
 cv2.destroyAllWindows()

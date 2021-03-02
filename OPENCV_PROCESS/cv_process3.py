6c67aef0937d425a7cdc15d47eaf63a66f0dc573
logic-three-body <1552451580@qq.com>
2021-03-02 16:39:22 +0800

    GaussianBlur参数意义

--- OPENCV_PROCESS/cv_process3.py
+++ OPENCV_PROCESS/cv_process3.py
@@ -4,7 +4,7 @@ print(cv2.__file__)
 cap = cv2.VideoCapture(0)#注意如果为1imshow会崩溃
 while True:
     ret, im = cap.read()
-    blur = cv2.GaussianBlur(im, (0, 0), 5)
+    blur = cv2.GaussianBlur(im, (0, 0), 5)# kernel_size[高斯内核]->(0,0) sigma[标准差]->5 refer: https://blog.csdn.net/wuqindeyunque/article/details/103694900?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.control&dist_request_id=&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.control
     cv2.imshow('video test', blur)
     key = cv2.waitKey(10)
     if key == 27:
